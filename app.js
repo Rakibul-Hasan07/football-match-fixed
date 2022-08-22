22c49f22ad436168bbb77c8f5d2577765468fdbc
shihab00027 <shihabhasan981@gmail.com>
2022-08-22 23:23:20 +0600

    Header Update

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ function showPlayer(playerCart) {
     ulNameAdded.innerHTML = '';
     for (let i = 0; i < playerCart.length; i++) {
         const name = playerArray[i].playerName;
-        //    console.log(name);
 
         const tr = document.createElement('tr');
         tr.innerHTML = `
@@ -13,37 +12,37 @@ function showPlayer(playerCart) {
        <td>${name}</td>
        `;
         ulNameAdded.appendChild(tr);
-        // console.log(ulNameAdded);
+        
     }
-    // console.log(playerCart);
+    
 }
 function clickToPlayerAdded(btnValue) {
     const playerName = btnValue.parentNode.parentNode.children[0].innerText;
-    // console.log(playerName);
+
 
     const playerObject = {
         playerName: playerName
     }
-    // console.log(playerObject);
+
     playerArray.push(playerObject);
     showPlayer(playerArray);
     const playerLength = playerArray.length;
-    // console.log(playerLength);
+
     document.getElementById('calculate-btn').addEventListener('click', function () {
         const perPlayerAmount = document.getElementById('per-player-amount');
         const playerAmount = perPlayerAmount.value;
-        // console.log(playerAmount);
+
         const amount = parseFloat(playerAmount);
         const additon = amount * playerLength;
-        // console.log(additon);
+
         const playerExpense = document.getElementById('player-expense');
         playerExpense.innerText = additon;
     })
-    document.getElementById('calculate-total-btn').addEventListener('click', function(){
+    document.getElementById('calculate-total-btn').addEventListener('click', function () {
         const managerAmount = document.getElementById('manager-amount');
         const managerAmountString = managerAmount.value;
         const managerAmountConvert = parseFloat(managerAmountString);
-        
+
         const coachAmount = document.getElementById('coach-amount');
         const coachAmountString = coachAmount.value;
         const coachAmountConvert = parseFloat(coachAmountString);
@@ -53,8 +52,8 @@ function clickToPlayerAdded(btnValue) {
         const sentPlayerAmountConvert = parseFloat(sentPlayerAmount);
 
         const totalAmount = sentPlayerAmountConvert + managerAmountConvert + coachAmountConvert;
-        
+
         document.getElementById('total-amount').innerText = totalAmount;
-        
+
     })
 }
